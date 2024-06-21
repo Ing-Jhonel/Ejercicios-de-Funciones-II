1b85ec134e3965e222b41b873707bd8988546062
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-21 10:33:03 -0500

    2.2 modificacion: mejore interfaz || agregue ejercicio 2 numeros aleatorios

--- 2 numeros aleatorios.cpp
+++ 2 numeros aleatorios.cpp
@@ -5,8 +5,8 @@
 //aleatorios entre 0 y un número máximo especificado por el usuario. El programa solicitará al usuario
 //la cantidad de números aleatorios que desea generar y el valor máximo permitido. Utiliza la función
 //rand de la librería cstdlib para generar los números aleatorios.
-//ENTRADA|| cantidadnumeros: cantidad de numeros a generar  vm: valor maximo o limite
-//SALIDA|| n1, n2, n3 .... nn: numeros generados aleatoriamente
+//ENTRADA|| cantidadnumeros: cantidad de numeros a generar  limitemaximo: valor maximo o limite
+//SALIDA|| numerogenerado1, numerogenerado2, numerogenerado3 .... nn: numeros generados aleatoriamente
 
 using namespace std;
 
@@ -14,14 +14,16 @@ int main() {
 	
 	srand(time(0)); 
 	int cantidadnumeros, limitemaximo, numerogenerado;
+	cout << "GENERADOR DE NUMEROS ALEATORIOS" << endl;
+	cout << "--------------------------------" << endl << endl;
 	cout << "Ingrese el limite maximo: ";
 	cin >> limitemaximo;
 		
 	cout << "Indique cuantos numeros quiere ver entre el 0 y " << limitemaximo << " : ";
 	cin >> cantidadnumeros;
 	cout << endl;
-
-	for(int i=1; i<=cantidadnumeros; i++){
+	
+ 	for(int i=1; i<=cantidadnumeros; i++){
 		numerogenerado = rand() % limitemaximo + 1;
 		cout << numerogenerado << " ";
 	}
