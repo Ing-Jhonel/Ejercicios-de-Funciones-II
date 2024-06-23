e1efaec1c3e59e6eaf6f44267efdd85751f98d7d
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-23 09:42:43 -0500

    4.1 modificacion: especifique variables || agregue ejercicio 4 raices

--- 4 raices.cpp
+++ 4 raices.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int main(){
 	
-	int a, b, c, r1, r2, d;
+	int a, b, c, raiz1, raiz2, discriminante;
 	cout << "Indique el termino cuadratico (a): ";
 	cin >> a;
 	cout << "Indique el termino lineal (b): ";
 	cin >> b;
 	cout << "Indique el termino independiente (c): ";
 	cin >> c;
-	d=b*b - 4*a*c;
-	if(d<0){
+	discriminante=b*b - 4*a*c;
+	if(discriminante<0){
 		cout << "Sus raices no son numeros reales. Intente de nuevo." << endl;
-	} else if(d==0){
-		r1 = (-b+sqrt(d))/2*a;
-		cout << "La raiz de la ecuacion cuadratica es " << r1 << endl;
+	} else if(discriminante==0){
+		raiz1 = (-b+sqrt(discriminante))/2*a;
+		cout << "La raiz de la ecuacion cuadratica es " << raiz1 << endl;
 	} else {
-		r1 = (-b+sqrt(d))/2*a;
-		r2 = (-b-sqrt(d))/2*a;
-		cout << "Las raices de la ecuacion cuadratica son " << r1 << " y " << r2 << endl;
+		raiz1 = (-b+sqrt(discriminante))/2*a;
+		raiz2 = (-b-sqrt(discriminante))/2*a;
+		cout << "Las raices de la ecuacion cuadratica son " << raiz1 << " y " << raiz2 << endl;
 	}
 		
 	return 0;
