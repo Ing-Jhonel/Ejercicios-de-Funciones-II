f632630d6e544bc69e17595f29a91a041714c7fa
Jhonel <jgapazap@unjbg.edu.pe>
2024-06-26 10:13:56 -0500

    3.4 modificacion: mejore interfaz || Agregue ejercicio 3 hipotenusa

--- 3 hipotenusa.cpp
+++ 3 hipotenusa.cpp
@@ -12,14 +12,17 @@ int main(){
 	
 	double cateto1, cateto2, hipotenusa;
 	cout << "CALCULA LA HIPOTENUSA DE UN TRIANGULO RECTANGULO" << endl;
-	cout << "************************************************" << endl << endl;
-	cout << "Indique un cateto: ";
+	cout << "************************************************" << endl;
+	cout << "Teorema de pitagoras (a^2  +  b^2  =  c^2)" << endl;
+	cout << "==========================================" << endl << endl;
+	cout << "Indique un cateto (a): ";
 	cin >> cateto1;
-	cout << "Indique el otro cateto: ";
+	cout << endl;
+	cout << "Indique el otro cateto (b): ";
 	cin >> cateto2;
 	hipotenusa = sqrt(cateto1*cateto1 + cateto2*cateto2); // con el sqrt hallamos la raiz de un numero
 	cout << endl;
 	cout << "--------------------------------" << endl << endl;
- 	cout << "La hipotenusa es: " << hipotenusa << endl;
+ 	cout << "La hipotenusa es (c): " << hipotenusa << endl;
 	return 0;
 }
